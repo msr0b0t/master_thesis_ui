77471e18cb2e0bc62b5c416d71955baced170ee4
Mary Kouvela <marykouvela@gmail.com>
2020-01-24 20:50:56 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
-import { Form, Header, Grid, Progress, Button, Modal, Dropdown, Checkbox } from "semantic-ui-react";
+import { Form, Header, Grid, Progress, Button, Modal, Dropdown, Checkbox, Icon } from "semantic-ui-react";
 import useFetch from "use-http";
 
 import ReasonList from "./ReasonList";
 
 function App() {
 	const [username, setUsername] = useState("");
-	const [request, response] = useFetch(`https://bot-detective.herokuapp.com/predict/${username.startsWith("@") ? username.slice(1) : username}`);
+	const [request, response] = useFetch("https://bot-detective.herokuapp.com");
+	// const [request, response] = useFetch("http://localhost:5000");
+
+	const [dropdownOption, setDropdownOption] = useState(null);
+	const [reasons, setReasons] = useState(new Map());
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const options = [
 		{ key: 1, text: "Human", value: 1 },
@@ -15,7 +20,15 @@ function App() {
 		{ key: 4, text: "Organization", value: 4 },
 	];
 
-	console.log(response.data);
+	React.useEffect(() => {
+		if (response.data) {
+			const tmp = new Map();
+			response.data.pos.forEach((e) => tmp.set(e, true));
+			response.data.neg.forEach((e) => tmp.set(e, true));
+			setDropdownOption(null);
+			setReasons(tmp);
+		}
+	}, [response.data]);
 
 	return (
 		<Grid divided="vertically" style={{ margin: "7rem" }}>
@@ -23,7 +36,7 @@ function App() {
 				<Grid.Column>
 					<Form
 						loading={request.loading}
-						onSubmit={() => request.get()}
+						onSubmit={() => request.get(`/predict/${username.startsWith("@") ? username.slice(1) : username}`)}
 						style={{ alignItems: "center", display: "flex", flexDirection: "column" }}
 					>
 						<img src={require("./bot_detective.svg")} alt="logo" />
@@ -65,22 +78,60 @@ function App() {
 						<Grid.Column>
 							<Header as="h2" color="teal" style={{ textAlign: "center" }}>Why NOT bot?</Header>
 							<ReasonList list={response.data.neg} color="teal" />
-							<Modal trigger={<Button color="purple" floated="right" style={{ marginTop: "11%" }}>Feedback</Button>}>
+							<Modal
+								open={isModalOpen}
+								onClose={() => setIsModalOpen(false)}
+								trigger={(
+									<Button
+										color="purple"
+										floated="right"
+										style={{ marginTop: "11%" }}
+										onClick={() => setIsModalOpen(true)}
+									>
+										{"Feedback"}
+									</Button>
+								)}
+							>
 								<Modal.Header>Help us improve</Modal.Header>
 								<Modal.Content>
 									<Modal.Description>
 										<h4>{`I believe the account ${username} is:`}</h4>
-										<Dropdown placeholder="Choose One" fluid selection options={options} />
+										<Dropdown
+											placeholder="Choose One"
+											fluid
+											selection
+											options={options}
+											value={dropdownOption}
+											onChange={(e, { value }) => setDropdownOption(value)}
+										/>
 										<h4>I aggre with:</h4>
-										{response.data.neg.map((value, i) => (
-											<Checkbox defaultChecked label={value} key={i} />
+										{[...reasons.entries()].map(([reason, isChecked], ind) => (
+											<Checkbox
+												checked={isChecked}
+												label={reason}
+												key={ind}
+												onChange={() => {
+													const tmp = new Map(reasons);
+													tmp.set(reason, !tmp.get(reason));
+													setReasons(tmp);
+												}}
+											/>
 										))}
-										{response.data.pos.map((value, i) => (
-											<Checkbox defaultChecked label={value} key={i} />
-										))}
-										<Button color="teal" floated="right" style={{ marginBottom: "2%" }}>Send</Button>
 									</Modal.Description>
 								</Modal.Content>
+								<Button
+									color="teal"
+									floated="right"
+									style={{ margin: "2%" }}
+									onClick={() => {
+										setIsModalOpen(false);
+										const userSelection = options.find((el) => el.value === dropdownOption) && options.find((el) => el.value === dropdownOption).text;
+										request.post("/feedback", { user_selection: userSelection, reasons: [...reasons], analysis_id: response.data.analysis_id });
+									}}
+								>
+									<Icon name="checkmark" />
+									{"  Send"}
+								</Button>
 							</Modal>
 						</Grid.Column>
 					</Grid.Row>
